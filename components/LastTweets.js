72ddacd879e3896abf1e1c60ecd0668dab863c93
Louis Catteau <louiscatteauo@gmail.com>
2024-05-03 10:24:37 +0200

    creation fonction send tweet

--- components/LastTweets.js
+++ components/LastTweets.js
@@ -10,9 +10,9 @@ const time='time'
   return (
     <div className={styles.main}>
       <div className={styles.user}>
-        <img className={styles.userLogo} src={`/${props.Firstname}.png`} alt="Logo" />
-        <p className={styles.Firstname}>{props.Firstname}  </p>
-        <p className={styles.Username}>@{props.Username} - {time}</p>
+        <img className={styles.userLogo} src={`/${props.firstname}.png`} alt="Logo" />
+        <p className={styles.Firstname}>{props.firstname}  </p>
+        <p className={styles.Username}>@{props.username} - {time}</p>
       </div>
       <div className={styles.message}>{props.message}</div>
       <FontAwesomeIcon icon={faHeart} className={styles.like} />{props.nbLike}</div>
