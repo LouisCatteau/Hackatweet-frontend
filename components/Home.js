c3c4dcde95a9474082ee7fd4c505cbc80c3eab1a
Louis Catteau <louiscatteauo@gmail.com>
2024-05-03 11:50:19 +0200

    install next-router

--- components/Home.js
+++ components/Home.js
@@ -8,31 +8,32 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useRouter } from 'next/router'
 
 
+
 function Home() {
-  const router = useRouter()
   const [message, setmessage] = useState('');
   const [isHome, setIsHome] = useState(true);
   const [hashtag, setHashtag] = useState('');
   const trends = useSelector((state) => state.trends.value);
   const [tweets, settweets] = useState([]);
   let allTweets = []
-	const user = useSelector((state) => state.user.value);
-  
-  router.push('/login')
+  const user = useSelector((state) => state.user.value);
+  /*const router = useRouter()
+  router.push('/login')*/
+
 
   const clickOnTrend = (trendName) => {
     setIsHome(false);
     setHashtag(trendName);
   };
-  
+
   const handleLogoClick = () => {
     setIsHome(true);
   }
 
   const trendsToDisplay = trends.map((trend, i) => {
-    return <Trend key={i} name={trend.name}  number={trend.number} clickOnTrend={clickOnTrend}/>;
+    return <Trend key={i} name={trend.name} number={trend.number} clickOnTrend={clickOnTrend} />;
   });
-  
+
   useEffect(() => {
     fetch('http://localhost:3000/tweets/allTweets')
       .then(response => response.json())
@@ -41,7 +42,7 @@ function Home() {
 
   const sendTweet = () => {
     const date = Date.now()
-    const tweet = { date: date, message: message, token: user.token  }
+    const tweet = { date: date, message: message, token: user.token }
     fetch('http://localhost:3000/tweets/newTweet', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
@@ -57,56 +58,56 @@ function Home() {
   })
 
 
-return (
-  <div className={styles.main}>
+  return (
+    <div className={styles.main}>
 
-    <div className={styles.leftBanner}>
-      <img className={styles.logo} src='/logo-twitter.png' alt="Logo" />
-      <div className={styles.profile}>
-        <div className={styles.user}>
-          <img className={styles.userLogo} src='/youtube.png' alt="Logo" />
-          <div>
-            <p className={styles.Firstname}>Youtube</p>
-            <p className={styles.Username}>@SimplyTheBest</p>
-          </div>
-        </div>
-        <button className={styles.logout}>Logout</button>
-      </div>
-    </div>
-
-    <div className={styles.content}>
-    {isHome && (
-          <div className={styles.sendTweet}>
-          <h2 className={styles.h2}>Home</h2>
-          <div className={styles.message}>
-            <input className={styles.addMessage} onChange={(e) => setmessage(e.target.value)} value={message}></input>
-            <div className={styles.button}>
-              <span>{message.length} / 280 </span>
-              <button className={styles.buttonSend} onClick={() => sendTweet()}>Tweet</button>
+      <div className={styles.leftBanner}>
+        <img className={styles.logo} src='/logo-twitter.png' alt="Logo" onClick={()=>handleLogoClick()} />
+        <div className={styles.profile}>
+          <div className={styles.user}>
+            <img className={styles.userLogo} src='/youtube.png' alt="Logo" />
+            <div>
+              <p className={styles.Firstname}>Youtube</p>
+              <p className={styles.Username}>@SimplyTheBest</p>
             </div>
           </div>
+          <button className={styles.logout}>Logout</button>
         </div>
+      </div>
+
+      <div className={styles.content}>
+        {isHome && (
+          <div className={styles.sendTweet}>
+            <h2 className={styles.h2}>Home</h2>
+            <div className={styles.message}>
+              <input className={styles.addMessage} onChange={(e) => setmessage(e.target.value)} value={message}></input>
+              <div className={styles.button}>
+                <span>{message.length} / 280 </span>
+                <button className={styles.buttonSend} onClick={() => sendTweet()}>Tweet</button>
+              </div>
+            </div>
+          </div>
         )}
         {!isHome && (
           <div className={styles.hashtag}>
-          <h2 className={styles.h2}>Hashtag</h2>
-          <div className={styles.message}>
-            <input className={styles.searchHastag} onChange={(e) => setHashtag(e.target.value)} value={hashtag}></input>
+            <h2 className={styles.h2}>Hashtag</h2>
+            <div className={styles.message}>
+              <input className={styles.searchHastag} onChange={(e) => setHashtag(e.target.value)} value={hashtag}></input>
+            </div>
           </div>
-        </div>
         )}
-      <div className={styles.tweetContainer}>
-        {allTweets}
+        <div className={styles.tweetContainer}>
+          {allTweets}
+        </div>
       </div>
-    </div>
 
-    <div className={styles.trends}>
-      <h2 className={styles.h2} >Trends</h2>
-      <div className={styles.trendsContainer}>
-        <Trend />
+      <div className={styles.trends}>
+        <h2 className={styles.h2} >Trends</h2>
+        <div className={styles.trendsContainer}>
+          <Trend />
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
 }
 export default Home;
